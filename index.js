9f0d29befbf0bb71a410157585f3b9828b744750
nathancrank <me@nathancrank.com>
2019-04-16 14:45:30 -0400

    testing crash issues

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const client = new Twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token: process.env.ACCESS_TOKEN_KEY,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
